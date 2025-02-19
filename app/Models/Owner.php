21b97d257bd6313701d5023a5fe27cc3a887b070
AlbertoCon <albertocon209@gmail.com>
2025-02-19 10:30:22 -0600

    Muchas cosas xd

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -1,10 +1,15 @@
 <?php
 
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
 
 class Owner extends Model
 {
-    //
+    public function patients(): HasMany //aqui en lo verde se pone el nombre en plural que las migraciones generaron
+    {
+        return $this->hasMany(Patient::class); //aqui en lo amarillo se pone el nonomre en singular donde decidimos llamarlo en la terminal
+    }
 }
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Patient extends Model
 {
-    //
+    public function owner(): BelongsTo //esto significa que solo puede tener un dueño
+    {
+        return $this->belongsTo(Owner::class);
+    }
+ 
+    public function treatments(): HasMany //esto signidica que puede tener muchos tratamientos
+    {
+        return $this->hasMany(Treatment::class);
+    }
+
 }
